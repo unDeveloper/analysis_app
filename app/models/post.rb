dd0a6cc493f71f42b04080f35ee92fa9fcf4c23a
Hugo Lopez <hugo.lopez@bigcommerce.com>
2020-09-15 00:19:36 -0500

    changing the content type validation to text/plain to accept .txt

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
     has_many_attached :files
 
-    validates :files, presence: true, blob: { content_type: ['application/text'], max_size: 15.kilobyte,
-        min_size: 1.kilobyte }
+    validates :files, presence: true, blob: { content_type: ['text/plain'], max_size: 15.kilobyte }
 end
