83a97ff93aa1e61a2d8fdab917ab14dc3889c3bb
Hugo Lopez <hugo.lopez@bigcommerce.com>
2020-09-15 00:13:42 -0500

    validator active stogare Gem added and valite just for txt and max 15 Kb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
     has_many_attached :files
+
+    validates :files, presence: true, blob: { content_type: ['application/text'], max_size: 15.kilobyte,
+        min_size: 1.kilobyte }
 end
