18bb5ce1c190c6d0e1157e2c6c1f84c9a4e36da3
Hugo Lopez <hugo.lopez@bigcommerce.com>
2020-09-14 22:01:12 -0500

    adding domain validation in sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  APPROVED_DOMAINS = ["envato.com", "envato.mx"]
+
+  validates :email, presence: true, if: :domain_check
+
+  def domain_check
+    unless APPROVED_DOMAINS.any? { |word| email.end_with?(word)}
+      errors.add(:email, "is not from a valid domain")
+    end
+  end         
 end
